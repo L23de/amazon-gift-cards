d541d2904663247ebb02821e07cb58150bc07036
Lester Huang <huangl302d@gmail.com>
2023-12-21 02:11:02 -0700

    add chase 5/24 calculator

--- common/driver.py
+++ common/driver.py
@@ -6,12 +6,10 @@ ENV = os.environ
 EDGE_USER_DATA_DIR = ENV.get("EDGE_USER_DATA_DIR")
 EDGE_BINARY_LOCATION = ENV.get("EDGE_BINARY_LOCATION")
 
-
-
 service = webdriver.EdgeService(executable_path="./msedgedriver.exe")
 options = webdriver.EdgeOptions()
 options.add_argument(f"user-data-dir={EDGE_USER_DATA_DIR}")
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.binary_location= EDGE_BINARY_LOCATION
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.binary_location = EDGE_BINARY_LOCATION
 
 driver = webdriver.Edge(options=options, service=service)
